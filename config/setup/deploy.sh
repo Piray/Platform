ea05a1de8ac857d006f6bc815e41014d315b1d93
Dachi Chang <femc7488@gmail.com>
2014-03-17 14:26:34 +0800

    change deploy path

--- config/setup/deploy.sh
+++ config/setup/deploy.sh
@@ -4,14 +4,15 @@ PLATFORM_FRAMEWORK="../../library"
 
 # install or update platform framework
 THIS_DIR=`pwd`
-if [ -d "$PLATFORM_FRAMEWORK/vendor" ]; then
-    cd $PLATFORM_FRAMEWORK; composer update
+if [ -d "$PLATFORM_FRAMEWORK/composer/vendor" ]; then
+    cd $PLATFORM_FRAMEWORK/composer; composer update
 else
-    cd $PLATFORM_FRAMEWORK; composer install
+    cd $PLATFORM_FRAMEWORK/composer; composer install
 fi
 cd $THIS_DIR
 
 # create assets link
 ln -snf ../../assets ../../public/assets/platform
-ln -snf ../../library/vendor ../../public/assets/vendor
+ln -snf ../../library/composer/vendor ../../public/assets/vendor
+ln -snf ../../library/opensource ../../public/assets/opensource
 
